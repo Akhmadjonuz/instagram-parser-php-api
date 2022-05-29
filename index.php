9b4400b9cb6415b47073a9b24d1777c16e7044fa
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-05-29 16:23:20 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
 
+# Ushbu kod Ahmadjon Dadabayev tomonidan qaytadan yozib chiqarildi. uitc.uz platformasi uchun bajarildi. 
+# Mualliflik huquqi saqlansin!
+# dadabayev.uz
+# +998902224311
+# @Akhmadjon
+
+# This code was rewritten by Ahmadjon Dadabayev. Done for the uitc.uz platform.
+# Copyright reserved!
+# dadabayev.uz
+# +998902224311
+# @Akhmadjon
+
+# Этот код был переписан Ахмаджоном Дадабаевым. Сделано для платформы uitc.uz
+# Авторские права защищены!
+# dadabayev.uz
+# +998902224311
+# @Akhmadjon
+
 function insta($link){
 
 $fields = ['link' => $link];
