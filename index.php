26d15addccc5bd1d4889b147d24da6b342fda96f
Ahmadjon Dadabayev <98649872+Akhmadjonuz@users.noreply.github.com>
2022-07-02 04:29:08 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,22 @@
 <?php
+@$_url = $_GET['url'];
+@$get = file_get_contents("https://api.sssgram.com/st-tik/ins/dl?url=" . $_url);
+@$array = json_decode($get, true);
 
+@$result = [];
+@$i = 0;
 
-function insta($link){
-
-$fields = ['link' => $link];
-
-$url = "https://instasaver.ru:443/api/post-content";
-$ch = curl_init();
-curl_setopt_array($ch, [
-  CURLOPT_URL => $url,
-  CURLOPT_POST => count($fields),
-  CURLOPT_POSTFIELDS => http_build_query($fields),
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_CUSTOMREQUEST => 'POST',
-  CURLOPT_ENCODING => "UTF-8",
-  CURLOPT_USERAGENT => "Mozilla/5.0 (Linux; Android 7.0; Nexus 6 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Mobile Safari/537.36"
-    ]);
-$result = curl_exec($ch);
-curl_close($ch);
-return $result;
+foreach($array as $key){
+    $url = str_replace('\\', '', $array['result']['insBos'][$i]['url']);
+    $type = $array['result']['insBos'][$i]['type'];
+    if($type == 'jpg'){
+        array_push($result,['type' => 'image', 'url' => $url]);
+    }elseif($type == 'mp4'){
+        array_push($result,['type' => 'video', 'url' => $url]);
+    }
+    $i++;
 }
 
-print_r (insta($_GET['url']));
+echo json_encode($result);
 exit;
 ?>
